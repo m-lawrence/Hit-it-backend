2d86cf1d459dba2bb662c8660c6d6a9b6d7a0885
Melissa Lawrence <mlawrence537@gmail.com>
2021-05-19 11:40:24 -0400

    switched to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_19_140433) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "band_users", force: :cascade do |t|
     t.string "email"
     t.string "password"
