d35aefd84ff76dab0a9efa026c7cdb6db4baeb82
Melissa Lawrence <mlawrence537@gmail.com>
2021-05-19 11:57:29 -0400

    added cors and reset pk sequence gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Venue.destroy_all 
+Show.destroy_all
+Tour.destroy_all
+BandUser.destroy_all
+
+Venue.reset_pk_sequence
+Show.reset_pk_sequence
+Tour.reset_pk_sequence
+BandUser.reset_pk_sequence
 
 # AVL Venues
 
