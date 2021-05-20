f9ae03d53f74e4bb63ae200ae049d3edddc48943
Melissa Lawrence <mlawrence537@gmail.com>
2021-05-20 14:02:09 -0400

    nested serializers

--- app/serializers/band_user_serializer.rb
+++ app/serializers/band_user_serializer.rb
@@ -1,4 +1,5 @@
 class BandUserSerializer < ActiveModel::Serializer
   has_many :tours
+  # has_many :shows, through: :tours
   attributes :id, :email, :password, :name, :website, :facebook, :music_link, :genre, :location, :image, :bio, :band_members, :spotify
 end
